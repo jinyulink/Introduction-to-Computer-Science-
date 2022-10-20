3f92854318a29c66830df3267b77f20cee196f95
jinyulink <jinyulin34@gmail.com>
2022-10-21 02:12:43 +0800

    week4

--- Lab_Practice_3/Practice03.py
+++ Lab_Practice_3/Practice03.py
@@ -8,14 +8,14 @@ Course 2022-CE1003-A
 def CreatePyramid(lever_int):
     res_string=""
     for i in range(1,lever_int+1):
-        res+=(" "*(lever_int-i))
-        res+=("*"*(2*i-1))
-        if i!=lever_int+1:
-            res+=('\n')
-    return res
+        res_string+=(" "*(lever_int-i))
+        res_string+=("*"*(2*i-1))
+        if i!=lever_int:
+            res_string+=('\n')
+    return res_string
 
-file=open("input.txt","r")
+file=open("Lab_Practice_3/input.txt","r")
 level_int=file.read()
-file=open("output.txt","w")
+file=open("Lab_Practice_3/output.txt","w")
 file.write(CreatePyramid(int(level_int)))
 file.close()
